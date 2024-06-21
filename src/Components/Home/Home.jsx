6875622ae4066b3061f40e6b5725e6a7a19c9e85
Sumit kumar <cflip3913@gmail.com>
2024-06-21 12:02:50 +0530

    add Category section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
- import { Navbar } from "../Navbar/Navbar"
-
+import { Category } from "../Category/Category"
+import { Navbar } from "../Navbar/Navbar"
+ 
 import './Home.css'
 export const Home = () => {
     return (
         <div className="homeSection">
             <Navbar />
-          
+            <Category/>
+            
         </div>
     )
 }
