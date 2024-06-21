6018ce98a3d8734807d705a4fe4e2f9a53baca7b
Sumit kumar <cflip3913@gmail.com>
2024-06-21 13:33:37 +0530

    Product Slider and Product Cards components

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,15 @@
 import { Category } from "../Category/Category"
 import { Navbar } from "../Navbar/Navbar"
- 
+import { ProductCards } from "../ProductCards/ProductCards"
+import { ProductSlider } from "../ProductSlider/ProductSlider"
 import './Home.css'
 export const Home = () => {
     return (
         <div className="homeSection">
             <Navbar />
             <Category/>
-            
+            <ProductSlider/>
+            <ProductCards/>
         </div>
     )
 }
